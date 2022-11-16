fa9ea032b579234e70f547bda04d10760e71203c
Amelia <amelia.ardath@gmail.com>
2022-11-15 18:34:41 -0800

    Add basic main, detail, and error pages

---
+++ desktop-exporter/app/routes/main-view.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function MainView() {
+    return (
+        <>
+            <ul>
+                <li>
+                    <Link to={`traces/1`}>Item 1</Link>
+                </li>
+                <li>
+                    <Link to={`traces/2`}>Item 2</Link>
+                </li>
+                <li>
+                    <Link to={`traces/3`}>Item 3</Link>
+                </li>
+            </ul>
+        </>
+    )
+}
