923a59ab3859b9e409dfff272eebd5e9f9cd3adf
Amelia Ardath <56372758+CtrlSpice@users.noreply.github.com>
2022-11-16 18:57:40 -0800

    render trace summaries in a table (#24)
    ![MainView](https://user-images.githubusercontent.com/56372758/202332262-22c4bec6-198c-457f-a6f9-be53483d2da4.jpg)
    ![DALL·E 2022-11-16 17 34 11 - A cute axolotl flipping a table, digital
    art](https://user-images.githubusercontent.com/56372758/202332276-775a7735-fe05-4955-a886-84d9f5647525.png)

--- desktop-exporter/app/routes/main-view.jsx
+++ desktop-exporter/app/routes/main-view.jsx
@@ -12,19 +12,31 @@ export default function MainView() {
     console.log(traceSummaries);
 
     const summaries = traceSummaries.map((summary) => (
-        <li key={summary.traceID}>
-            <Link to={`traces/${summary.traceID}`}>
-                Trace: {summary.traceID}
-            </Link>
-        </li>
+        <tr key={summary.traceID}>
+            <td><Link to={`traces/${summary.traceID}`}>{summary.traceID}</Link></td>
+            <td>{summary.spanCount}</td>
+            <td>{summary.durationMS}</td>
+        </tr>
     ));
 
     return (
         <>
             {traceSummaries.length ? (
-                <ul>{summaries}</ul>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Trace ID</th>
+                            <th>Span Count</th>
+                            <th>Duration (MS)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {summaries}
+                    </tbody>
+                </table>
+
             ) : (
-                <p><i>No traces</i></p>
+                    <p>No traces yet</p>
             )}
         </>
     )
