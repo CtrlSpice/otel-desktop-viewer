0c03f5c9ae69b4575e5d4f0f9ab912b6eb858ce4
Amelia Ardath <56372758+CtrlSpice@users.noreply.github.com>
2022-11-22 15:53:36 -0800

    Basic layout for the application (#29)
    Complete with collapsible sidebar. Horrible colours for ease of
    visualization only - please ignore.
    
    
    ![traces:{id}](https://user-images.githubusercontent.com/56372758/203408039-ec8786b9-62c7-48ce-9e1f-b93c58a8a44d.jpg)
    ![traces:{id} - collapse
    sidebar](https://user-images.githubusercontent.com/56372758/203408100-9e537df3-c535-441d-9c14-69874325fa35.jpg)

--- desktop-exporter/app/routes/main-view.jsx
+++ desktop-exporter/app/routes/main-view.jsx
@@ -1,5 +1,8 @@
-import React from 'react';
-import { Link, useLoaderData } from "react-router-dom";
+import React from "react";
+import { Outlet, NavLink, useLoaderData } from "react-router-dom";
+import { FixedSizeList } from "react-window";
+import { useToggle } from "usehooks-ts";
+
 
 export async function mainLoader() {
     const response = await fetch("/api/traces");
@@ -7,36 +10,57 @@ export async function mainLoader() {
     return traceSummaries;
 }
 
-export default function MainView() {
-    const { traceSummaries } = useLoaderData();
 
-    const summaries = traceSummaries.map((summary) => (
-        <tr key={summary.traceID}>
-            <td><Link to={`traces/${summary.traceID}`}>{summary.traceID}</Link></td>
-            <td>{summary.spanCount}</td>
-            <td>{summary.durationMS}</td>
-        </tr>
-    ));
+function Row({ index, style, data }) {
+    return (
+        <NavLink to={`traces/${data[index].traceID}`} style={style}>
+            {data[index].traceID}
+        </NavLink>
+    );
+}
+
+
+function Sidebar(props) {
+    if (props.isClosed) {
+        return (
+            <div className="sidebar closed">
+                <button className="menuBtn" onClick={props.toggle}>
+                    Expand
+                </button>
+            </div>
+        );
+    }
+
+    const { traceSummaries } = useLoaderData();
+    return (
+        <div className="sidebar">
+            <button className="menuBtn" onClick={props.toggle}>
+                Collapse
+            </button>
+            <nav>
+                <FixedSizeList
+                    className="list"
+                    height={500}
+                    itemData={traceSummaries}
+                    itemCount={traceSummaries.length}
+                    itemSize={30}
+                    width={"100%"}
+                >
+                    {Row}
+                </FixedSizeList>
+            </nav>
+        </div>
+    );
+
+}
+
+export default function MainView() {
+    let [isClosed, toggleClosed] = useToggle();
 
     return (
-        <>
-            {traceSummaries.length ? (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Trace ID</th>
-                            <th>Span Count</th>
-                            <th>Duration (MS)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {summaries}
-                    </tbody>
-                </table>
-
-            ) : (
-                    <p>No traces yet</p>
-            )}
-        </>
+        <div className="container">
+            <Sidebar isClosed={isClosed} toggle={toggleClosed} />
+            <Outlet />
+        </div>
     )
 }
