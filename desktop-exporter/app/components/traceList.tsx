b00956584336819acc531ab5bfcf6a020bf1d5e5
Amelia <amelia.ardath@gmail.com>
2022-12-06 10:59:16 -0800

    make tracelist items look moderately less terrible

--- desktop-exporter/app/components/traceList.tsx
+++ desktop-exporter/app/components/traceList.tsx
@@ -1,7 +1,19 @@
 import React, { useRef } from "react";
 import { FixedSizeList } from "react-window";
 import { NavLink } from "react-router-dom";
-import { Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Heading,
+  LinkBox,
+  LinkOverlay,
+  Stack,
+  StackDivider,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { useSize } from "@chakra-ui/react-use-size";
 
 import { TraceSummary } from "../types/api-types";
@@ -13,13 +25,40 @@ type RowProps = {
 };
 
 function Row({ index, style, data }: RowProps) {
+  const backgroundColour = useColorModeValue(
+    "whiteAlpha.700",
+    "whiteAlpha.400",
+  );
+
+  const selectedColour = useColorModeValue("teal.100", "teal.900");
+
   return (
-    <NavLink
-      to={`traces/${data[index].traceID}`}
-      style={style}
-    >
-      {data[index].traceID}
-    </NavLink>
+    <div style={style}>
+      <LinkBox
+        bgColor={backgroundColour}
+        height="100px"
+        padding="10px"
+        marginX="10px"
+        marginTop="10px"
+        rounded="md"
+        _selected={{ bgColor: { selectedColour } }} // This is too much curlies
+      >
+        <Heading
+          marginY="1"
+          noOfLines={1}
+          size="sm"
+        >
+          <LinkOverlay
+            as={NavLink}
+            to={`traces/${data[index].traceID}`}
+          >
+            {data[index].traceID}
+          </LinkOverlay>
+        </Heading>
+        <Text>Number of spans: {data[index].spanCount}</Text>
+        <Text>Duration: {data[index].spanCount}</Text>
+      </LinkBox>
+    </div>
   );
 }
 
@@ -40,7 +79,7 @@ export function TraceList(props: TraceListProps) {
         height={size ? size.height : 0}
         itemData={props.traceSummaries}
         itemCount={props.traceSummaries.length}
-        itemSize={50}
+        itemSize={110}
         width="100%"
       >
         {Row}
