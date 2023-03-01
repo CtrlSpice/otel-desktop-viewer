b7a84698ed1065b3a994a2269e8d02783dbc89f2
Amelia <amelia.ardath@gmail.com>
2023-03-01 15:12:21 -0800

    Add basic integration test to CI workflow

---
+++ .github/workflows/integration.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Launch the app in the background
+./../../otel-desktop-viewer &
+
+# Save the process ID to kill it later
+pid=$!
+
+# Wait a second for everything to boot up
+sleep 1
+
+# Send an example trace
+curl -is http://localhost:4318/v1/traces -X POST -H "Content-Type: application/json" -d @./span.json
+
+sleep 1
+
+# Check that a trace summary has been created, and the rootServiceName is correct
+response=$(curl 'http://localhost:8000/api/traces' -H "Content-Type: application/json")
+
+rootServiceName=$(jq '.traceSummaries[0].rootServiceName' <<< $response)
+
+if [ $rootServiceName == '"test-with-curl"' ]
+then
+    echo 'Exit status 0: All good.'
+    kill -15 $pid
+    exit 0
+else
+    echo 'Exit status 1: unexpected rootServiceName ' + $rootServiceName
+    kill -15 $pid
+    exit 1
+fi
