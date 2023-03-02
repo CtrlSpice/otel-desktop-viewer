1a97819e694a1a67e8f2e1dbc097261854cb0fed
Amelia <amelia.ardath@gmail.com>
2023-03-02 12:10:21 -0800

    Testing CI workflow.

--- .github/workflows/integration.sh
+++ .github/workflows/integration.sh
@@ -6,26 +6,30 @@
 # Save the process ID to kill it later
 pid=$!
 
-# Wait a second for everything to boot up
-sleep 5
+pwd
 
-# Send an example trace
-curl -i http://localhost:4318/v1/traces -X POST -H "Content-Type: application/json" -d @./span.json
+ls -la
 
-sleep 1
+# # Wait a second for everything to boot up
+# sleep 5
 
-# Check that a trace summary has been created, and the rootServiceName is correct
-response=$(curl 'http://localhost:8000/api/traces' -H "Content-Type: application/json")
+# # Send an example trace
+# curl -i http://localhost:4318/v1/traces -X POST -H "Content-Type: application/json" -d @./span.json
 
-rootServiceName=$(jq '.traceSummaries[0].rootServiceName' <<< $response)
+# sleep 1
 
-if [ $rootServiceName == '"test-with-curl"' ]
-then
-    echo 'Exit status 0: All good.'
-    kill -15 $pid
-    exit 0
-else
-    echo 'Exit status 1: unexpected rootServiceName ' + $rootServiceName
-    kill -15 $pid
-    exit 1
-fi
+# # Check that a trace summary has been created, and the rootServiceName is correct
+# response=$(curl 'http://localhost:8000/api/traces' -H "Content-Type: application/json")
+
+# rootServiceName=$(jq '.traceSummaries[0].rootServiceName' <<< $response)
+
+# if [ $rootServiceName == '"test-with-curl"' ]
+# then
+#     echo 'Exit status 0: All good.'
+#     kill -15 $pid
+#     exit 0
+# else
+#     echo 'Exit status 1: unexpected rootServiceName ' + $rootServiceName
+#     kill -15 $pid
+#     exit 1
+# fi
