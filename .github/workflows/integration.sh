db49ae0a85dcc8ddfb2f3ec9b8a9d4ebacfef1b9
Amelia <amelia.ardath@gmail.com>
2023-03-02 12:14:08 -0800

    Testing CI workflow.

--- .github/workflows/integration.sh
+++ .github/workflows/integration.sh
@@ -10,26 +10,26 @@ pwd
 
 ls -la
 
-# # Wait a second for everything to boot up
-# sleep 5
+# Wait a second for everything to boot up
+sleep 2
 
-# # Send an example trace
-# curl -i http://localhost:4318/v1/traces -X POST -H "Content-Type: application/json" -d @./span.json
+# Send an example trace
+curl -i http://localhost:4318/v1/traces -X POST -H "Content-Type: application/json" -d @./.github/workflows/span.json
 
-# sleep 1
+sleep 1
 
-# # Check that a trace summary has been created, and the rootServiceName is correct
-# response=$(curl 'http://localhost:8000/api/traces' -H "Content-Type: application/json")
+# Check that a trace summary has been created, and the rootServiceName is correct
+response=$(curl 'http://localhost:8000/api/traces' -H "Content-Type: application/json")
 
-# rootServiceName=$(jq '.traceSummaries[0].rootServiceName' <<< $response)
+rootServiceName=$(jq '.traceSummaries[0].rootServiceName' <<< $response)
 
-# if [ $rootServiceName == '"test-with-curl"' ]
-# then
-#     echo 'Exit status 0: All good.'
-#     kill -15 $pid
-#     exit 0
-# else
-#     echo 'Exit status 1: unexpected rootServiceName ' + $rootServiceName
-#     kill -15 $pid
-#     exit 1
-# fi
+if [ $rootServiceName == '"test-with-curl"' ]
+then
+    echo 'Exit status 0: All good.'
+    kill -15 $pid
+    exit 0
+else
+    echo 'Exit status 1: unexpected rootServiceName ' + $rootServiceName
+    kill -15 $pid
+    exit 1
+fi
